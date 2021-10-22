55fb1b6e2c54c81e59c28bc12597fc17cee71c86
Mathias V. Nielsen <1547127+math280h@users.noreply.github.com>
2021-10-22 17:16:29 +0200

    Remove reporting from Expect-CT

--- middleware/security.ts
+++ middleware/security.ts
@@ -1,6 +1,6 @@
 export default function (req: any, res: any, next: any) {
   res.setHeader('Content-Security-Policy', "default-src 'self' fonts.googleapis.com fonts.gstatic.com style-src 'self'")
-  res.setHeader('Expect-CT', 'report-uri="mailto:mvn@devpod.dk", enforce, max-age=86400')
+  res.setHeader('Expect-CT', 'enforce, max-age=86400')
   res.setHeader('X-Frame-Options', 'SAMEORIGIN')
   res.setHeader('X-XSS-Protection', '1; mode=block')
   res.setHeader('X-Content-Type-Options', 'nosniff')
